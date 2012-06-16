ecf0db88430b47b6e7d6db1094417ca037d6bdb8
Jeremy Dorn <jeremy@jeremydorn.com>
2012-06-16 11:51:56 -0700

    Initial commit of files.

---
+++ validator.js
@@ -0,0 +1,91 @@
+var formValidator = function(options) {
+	//extend the default options
+	this.options = this.defaults;
+	for(var i in options) {
+		this.options[i] = options[i];
+	}
+}
+
+//default configuration options
+//you can override these to change them globally
+formValidator.prototype.defaults = {
+	addError: function(field, message) {},
+	removeError: function(field) {},
+	rules: {},
+	getFieldKey: function(field) { return field; },
+	onErrors: function(errors, event) {}
+};
+
+formValidator.prototype.validateField = function(field, event) {
+	try {
+		this.runValidation(field, event);
+		this.options.removeError(field);
+		return true;
+	}
+	catch(message) {
+		this.options.addError(field, message);
+		
+		var errors = [
+			{
+				field: field,
+				message: message
+			}
+		];
+		
+		this.options.onErrors(errors, event);
+		
+		return false;
+	}
+}
+formValidator.prototype.validateFields = function(fields,event) {
+	var errors = [];
+	
+	console.log(fields);
+	
+	for(var i in fields) {
+		try {
+			this.runValidation(fields[i], event);
+			this.options.removeError(fields[i]);
+		}
+		catch(message) {
+			this.options.addError(fields[i], message);
+			
+			errors.push(
+				{
+					field: fields[i],
+					message: message
+				}
+			);
+		}
+	}
+	
+	if(errors.length) {
+		this.options.onErrors(errors, event);
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+formValidator.prototype.runValidation = function(field, event) {
+	var key = this.options.getFieldKey(field);
+	
+	//if a rule exists for this field, run it
+	//the rule will throw an exception if it fails
+	if(typeof this.options.rules[key] !== 'undefined') {
+		this.options.rules[key](field, event);
+	}
+	
+	return true;
+}
+
+formValidator.prototype.clearError = function(field) {
+	this.options.removeError(field);
+}
+
+formValidator.prototype.clearErrors = function(fields) {
+	for(var i in fields) {
+		this.options.removeError(fields[i]);
+	}
+}
